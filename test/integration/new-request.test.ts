4541ad4d3e5041fe0a58e9deb462b7107e35730e
Jan Macku <jamacku@redhat.com>
2023-03-27 10:07:38 +0200

    test: POST /requests - bad API key

--- test/integration/new-request.test.ts
+++ test/integration/new-request.test.ts
@@ -6,7 +6,22 @@ describe('Test Testing Farm POST /requests', () => {
   test.todo('response', async () => {
     const api = new TestingFarmAPI('https://api.dev.testing-farm.io/v0.1');
 
-    // const response = await api.newRequest( ... );
+    // const response = await api.newRequest({
+    //   api_key: 'api_key',
+    //   test: {},
+    // });
     // expect(response).toBeDefined();
+  });
+
+  test('bad API key', async () => {
+    const api = new TestingFarmAPI('https://api.dev.testing-farm.io/v0.1');
+
+    const response = api.newRequest({
+      api_key: 'api_key',
+      test: {},
+    });
+    await expect(response).rejects.toThrowErrorMatchingInlineSnapshot(
+      '"{\\"errors\\":{\\"test\\":\\"Only one of test type \'fmf\', \'script\' or \'sti\' can be specified in \'test\' field.\\"}}"'
+    );
   });
 });
