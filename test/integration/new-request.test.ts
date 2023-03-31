bb21d713bb5d856eb89dbff3422bd5fec5534eb9
Jan Macku <jamacku@redhat.com>
2023-03-31 16:15:55 +0200

    feat: add option to return unparsed REST API response

--- test/integration/new-request.test.ts
+++ test/integration/new-request.test.ts
@@ -3,14 +3,12 @@ import { expect, test, describe } from 'vitest';
 import TestingFarmAPI from '../../src/index';
 
 describe('Test Testing Farm POST /requests', () => {
-  test.todo('response', async () => {
+  test.todo('safe response', async () => {
     const api = new TestingFarmAPI('https://api.dev.testing-farm.io/v0.1');
+  });
 
-    // const response = await api.newRequest({
-    //   api_key: 'api_key',
-    //   test: {},
-    // });
-    // expect(response).toBeDefined();
+  test.todo('unsafe response', async () => {
+    const api = new TestingFarmAPI('https://api.dev.testing-farm.io/v0.1');
   });
 
   test('bad API key', async () => {
