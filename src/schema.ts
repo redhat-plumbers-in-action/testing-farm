7b4c813e46d7ce0b1e602a72859c417888e2681e
Jan Macku <jamacku@redhat.com>
2023-11-13 10:05:25 +0100

    fix: xunit schema and snapshot

--- src/schema.ts
+++ src/schema.ts
@@ -158,7 +158,7 @@ export const requestSchema = z.object({
   result: z.object({
     summary: z.union([z.string(), z.null()]),
     overall: z.string(),
-    xunit: z.string(),
+    xunit: z.string().nullable(),
   }),
   run: z.object({
     console: z.union([urlSchema, z.null()]),
