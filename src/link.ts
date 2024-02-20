33162245cc79de83a8f9a9a9cb52ac4846a7f66c
Jan Macku <jamacku@redhat.com>
2024-02-20 13:52:34 +0100

    feat: add support for query parameters in the URL

--- src/link.ts
+++ src/link.ts
@@ -30,8 +30,21 @@ export abstract class TestingFarmLink {
 
   protected abstract request(config: AxiosRequestConfig): Promise<unknown>;
 
-  protected buildURL(path: string): URL {
+  protected buildURL(path: string): URL;
+  protected buildURL<P extends Record<string, string>>(
+    path: string,
+    searchParams: P
+  ): URL;
+  protected buildURL<P extends Record<string, string>>(
+    path: string,
+    searchParams?: P
+  ): URL {
     let url = new URL(`${this.instance.pathname}/${path}`, this.instance);
+
+    if (searchParams) {
+      url.search = new URLSearchParams(searchParams).toString();
+    }
+
     return url;
   }
 
